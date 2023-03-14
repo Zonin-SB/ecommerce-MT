d773a62c78ba5033d0480bac02d94aa778dceb03
Zonin <zoninvakkom@gmail.com>
2023-03-14 20:36:05 +0530

    database changed to atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,7 +5,8 @@ const state = {
 };
 
 module.exports.connect = function (done) {
-  const url = "mongodb://localhost:27017";
+  const url =
+    "mongodb+srv://zonin:*Zonin%40143%23@cluster0.xgj4vzd.mongodb.net/ecommerce?retryWrites=true&w=majority";
   const dbname = "ecommerce";
 
   mongoClient.connect(url, (err, data) => {
